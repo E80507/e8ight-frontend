1837785b0349c21ab7e21ed9978e5209cc597dea
daggle-minseo <ms.choi@daggle.io>
2025-05-29 17:47:43 +0900

    feat: 공용 아이콘 버튼 컴포넌트 추가

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { LoadIcon } from "../shared/loading/loading";
+import Image from "next/image";
 
 const buttonVariants = cva(
   "flex w-full items-center justify-center whitespace-nowrap rounded-[100px] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:text-black/40",
@@ -18,9 +19,9 @@ const buttonVariants = cva(
       size: {
         default: "h-12 px-[15px] heading-4",
         sm: "h-[30px] max-w-max px-3 py-1 subtitle-3",
-        md: "w-[80px] h-[37px] px-4 py-2 title-s",
-        lg: "w-[120px] lg:w-[160px] h-[48px] px-6 py-3 title-s",
-        cta: "w-[240px] h-[59px] title-s",
+        md: "h-[37px] w-[80px] px-4 py-2 title-s",
+        lg: "h-[48px] w-[120px] px-6 py-3 title-s lg:w-[160px]",
+        cta: "h-[59px] w-[240px] title-s",
         icon: "size-9",
       },
       shape: {
@@ -47,7 +48,16 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { className, variant, size, shape, loading, loadColor, asChild = false, ...props },
+    {
+      className,
+      variant,
+      size,
+      shape,
+      loading,
+      loadColor,
+      asChild = false,
+      ...props
+    },
     ref,
   ) => {
     const Comp = asChild ? Slot : "button";
@@ -79,4 +89,74 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   },
 );
 Button.displayName = "Button";
-export { Button, buttonVariants };
+
+const iconButtonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap transition-colors",
+  {
+    variants: {
+      variant: {
+        normal: "",
+        outline:
+          "rounded-full border border-gray-200 bg-white hover:bg-gray-50 active:bg-gray-100 disabled:bg-gray-50",
+        fill: "rounded-full bg-white hover:bg-gray-50 active:bg-gray-100 disabled:bg-gray-50",
+      },
+      size: {
+        sm: "size-[24px]",
+        md: "size-[40px]",
+      },
+    },
+    defaultVariants: {
+      variant: "normal",
+      size: "md",
+    },
+  },
+);
+
+export interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof iconButtonVariants> {
+  asChild?: boolean;
+  src: string;
+  width: number;
+  height: number;
+  badgeContent?: string;
+}
+
+const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      src,
+      width,
+      height,
+      asChild = false,
+      disabled,
+      ...props
+    },
+    ref,
+  ) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <div className="inline-flex">
+        <Comp
+          className={cn(iconButtonVariants({ variant, size }), className)}
+          ref={ref}
+          disabled={disabled}
+          {...props}
+        >
+          <Image
+            src={src}
+            alt={`${src}-icon`}
+            width={width}
+            height={height}
+            className={disabled ? "opacity-20" : "opacity-100"}
+          />
+        </Comp>
+      </div>
+    );
+  },
+);
+IconButton.displayName = "IconButton";
+export { Button, buttonVariants, IconButton };
