80eb7a5d98e2a0e57e202eef5c7e7ae3a80a4b89
daggle-hyeran <hr.kim@daggle.io>
2025-05-29 18:47:52 +0900

    feat: 임시 커밋

--- components/shared/form/custom-input-field.tsx
+++ components/shared/form/custom-input-field.tsx
@@ -13,40 +13,38 @@ import {
 } from "../../ui/form";
 import { Input } from "../../ui/input";
 import { handlePhoneNumberInput } from "@/util/number";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface CustomInputFieldProps<T extends FieldValues> {
   form: UseFormReturn<T>;
   name: Path<T>;
   placeholder: string;
-  isValid?: boolean; // 유효성 검사 여부
-  validText?: string; // 유효성 검사 텍스트
   maxLength?: number; // 최대 입력 가능 글자 수
   noSpace?: boolean; // 첫 글자 공백 입력 불가 여부
   label?: string; // 라벨
   disabled?: boolean; // 비활성화 여부
   type?: "number" | "email" | "text" | "password" | "tel";
   className?: string; // 클래스명
-  noMessage?: boolean; // 메시지 미표시 여부
+  required?: boolean; // 필수 입력 여부
 }
 
 const CustomInputField = <T extends FieldValues>({
   form,
   name,
   placeholder,
-  isValid,
-  validText,
   maxLength,
   noSpace,
   label,
   disabled,
   type,
   className,
-  noMessage,
+  required
 }: CustomInputFieldProps<T>) => {
   const [currentType, setCurrentType] = useState(type);
   const [isExposedPassword, setExposed] = useState(false);
+  const error = form.formState.errors[name];
 
   // 값 변경 핸들러
   const onChangeValue = (
@@ -82,7 +80,9 @@ const CustomInputField = <T extends FieldValues>({
         <FormItem>
           {label ? (
             <FormLabel className="mb-3 web:mb-2" htmlFor={name}>
-              {label}
+              <div className={cn("relative", required && "required")}>
+                {label}
+              </div>
             </FormLabel>
           ) : (
             <FormLabel htmlFor={name} />
@@ -94,7 +94,7 @@ const CustomInputField = <T extends FieldValues>({
                 disabled={disabled}
                 id={name}
                 maxLength={maxLength}
-                className={`disabled:mt-3 disabled:bg-black/10 ${!isValid && validText ? "border-destructive focus-visible:border-destructive" : ""} ${className}`}
+                className={`disabled:mt-3 disabled:bg-black/10 ${error ? "border-destructive focus-visible:border-destructive" : ""} ${className}`}
                 {...field}
                 onChange={
                   noSpace
@@ -118,13 +118,10 @@ const CustomInputField = <T extends FieldValues>({
               )}
             </div>
           </FormControl>
-          {!noMessage && !isValid && validText && (
-            <div className="mt-2">
-              <FormMessage>
-                <span>{validText}</span>
-              </FormMessage>
-            </div>
-          )}
+
+          <div className="mt-2">
+            <FormMessage />
+          </div>
         </FormItem>
       )}
     />
