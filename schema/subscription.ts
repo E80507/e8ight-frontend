796182a14348a8d9e4ff071497b4ea84476d92fe
daggle-hyeran <hr.kim@daggle.io>
2025-05-30 04:59:35 +0900

    feat: 라벨 폰트 수정

--- schema/subscription.ts
+++ schema/subscription.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const subscriptionSchema = z
   .object({
    email: z
-    .string({ required_error: "항목을 입력해주세요." })
+    .string({ required_error: "이메일을 입력해주세요." })
     .email({ message: "유효한 이메일 주소를 입력해주세요." }),
   })
