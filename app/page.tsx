51e5a879659e1e59f0d0301af09f2e6a4e3675ec
daggle-hyeran <hr.kim@daggle.io>
2025-05-29 17:13:39 +0900

    feat: 아카이브 세션, 구독 세션 생성 및 반응형 작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 "use client";
 
+import ArchiveSection from "./main/archive-section";
+import SubscriptionSection from "./main/subscription-section";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* 아카이브 세션 */}
+      <ArchiveSection />
+
+      {/* 구독 세션 */}
+      <SubscriptionSection />
+    </div>
+  );
 };
+
 export default Home;
