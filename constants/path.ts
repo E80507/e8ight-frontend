cabbc1cea5c05a54ee73efcbdbda85491b893bef
daggle-minseo <ms.choi@daggle.io>
2025-05-29 14:47:56 +0900

    feat: 어드민 제외한 파일 경로 상수화

--- constants/path.ts
+++ constants/path.ts
@@ -1,24 +1,9 @@
-export const LOGIN_PAGE = "/login";
-export const MEMBER_PAGE = "/member";
+// 기술 콘텐츠
+export const TECH_INSIGHTS_PAGE = "/tech-insights";
+export const TECH_LIBRARY_PAGE = "/tech-library";
 
-// [피드 관리]
-export const ARTWORK_PAGE = "/artwork"; // 작품 목록 페이지
-export const NOTICE_PAGE = "/notice"; // 공지 목록 페이지
-export const REPORT_PAGE = "/report"; // 신고 목록 페이지
+// 다운로드
+export const DOWNLOADS_PAGE = "/downloads";
 
-// [결제 관리]
-export const POINT_CHARGE_PAGE = "/point/charge"; // 포인트 충전 목록 페이지
-export const POINT_USE_PAGE = "/point/use"; // 포인트 사용 목록 페이지
-
-// [주문 관리]
-export const DELIVERY_PAGE = "/delivery"; // 배송 목록 페이지
-export const ORDER_PAGE = "/order"; // 주문 츨력 페이지
-
-// [기본 관리]
-export const SETTING_PAGE = "/setting"; // 기본 관리 페이지
-
-// [관리자 계정 관리]
-export const ADMIN_ACCOUNT_PAGE = "/account"; // 관리자 계정 관리 페이지
-
-// 비밀번호 변경 페이지
-export const PASSWORD_RESET_PAGE = "/password/reset"; // 비밀번호 변경 페이지
+// 문의하기
+export const CONTACT_PAGE = "/contact";
