eb2f99eaac606a5a0a0fbefa618142fc8e8d613a
daggle-minseo <ms.choi@daggle.io>
2025-05-31 15:59:27 +0900

    fix: Input 컴포넌트 variant 속성 추가

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,25 +1,34 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: "default" | "bottom-border";
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type = "text", variant = "default", ...props }, ref) => {
+    const baseClass =
+      "flex w-full transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:border-black focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:text-black/40";
+
+    const variants = {
+      default:
+        "border rounded-md border-black/10 px-4 py-3 pretendard-body-2  placeholder:text-[#8B909C] ",
+      "bottom-border":
+        "border-b border-gray-strong py-4 pl-10 placeholder:text-label-alternative font-pretendard body-1",
+    };
+
     return (
       <input
-        type={type}
-        className={cn(
-          "flex border rounded-md w-full border-black/10 px-4 py-3 pretendard-body-2 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#8B909C] focus-visible:border-black focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:text-black/40",
-          className,
-        )}
         ref={ref}
+        type={type}
+        className={cn(baseClass, variants[variant], className)}
         {...props}
       />
     );
   },
 );
+
 Input.displayName = "Input";
 
 export { Input };
