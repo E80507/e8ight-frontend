751769e9e05d1b15b77136cae22f7d6ac8e36ec3
daggle-minseo <ms.choi@daggle.io>
2025-05-31 21:03:58 +0900

    fix: gnb와 drawer 충돌 해결

--- components/shared/layout/global-nav-bar.tsx
+++ components/shared/layout/global-nav-bar.tsx
@@ -4,9 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
-import { Drawer, DrawerContent, DrawerFooter } from "@/components/ui/drawer";
-import { IconButton } from "@/components/ui/button";
-import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { IconButton, Button } from "@/components/ui/button";
 import {
   TECH_INSIGHTS_PAGE,
   TECH_LIBRARY_PAGE,
@@ -18,9 +22,9 @@ import ExternalLinksNav from "@/components/shared/layout/external-links-nav";
 import ActionButtons from "@/components/shared/layout/action-buttons";
 
 const NAV_LINKS = [
-  { label: "Tech Library", path: `${TECH_LIBRARY_PAGE}` },
-  { label: "Tech Insight", path: `${TECH_INSIGHTS_PAGE}` },
-  { label: "Downloads", path: `${DOWNLOADS_PAGE}` },
+  { label: "Tech Library", path: TECH_LIBRARY_PAGE },
+  { label: "Tech Insight", path: TECH_INSIGHTS_PAGE },
+  { label: "Downloads", path: DOWNLOADS_PAGE },
 ];
 
 const GlobalNavBar = () => {
@@ -34,11 +38,15 @@ const GlobalNavBar = () => {
   return (
     <>
       {/* 홈페이지 데스크톱 GNB */}
-      <div
-        className={`relative z-[999] hidden w-full flex-col px-[120px] py-4 font-pretendard web:flex ${isContactPage ? "bg-[#FBFBFC]" : "mx-auto max-w-[1440px] bg-transparent"}`}
+      <header
+        className={`relative z-[999] hidden w-full flex-col px-[120px] py-4 font-pretendard web:flex ${
+          isContactPage
+            ? "bg-[#FBFBFC]"
+            : "mx-auto max-w-[1440px] bg-transparent"
+        }`}
       >
         <div className="flex items-center justify-between">
-          <Link prefetch={false} href="/">
+          <Link href="/">
             <Image
               src="/svg/logo.svg"
               alt={SERVICE_NAME}
@@ -55,9 +63,8 @@ const GlobalNavBar = () => {
             {NAV_LINKS.map(({ label, path: href }) => (
               <Link
                 key={href}
-                prefetch={false}
                 href={href}
-                className={`flex items-center gap-3 title-l`}
+                className="flex items-center gap-3 title-l"
               >
                 {label}
               </Link>
@@ -72,14 +79,16 @@ const GlobalNavBar = () => {
             상담문의
           </Button>
         </div>
-      </div>
+      </header>
 
       {/* 홈페이지 모바일 GNB */}
-      <div
-        className={`relative z-[999] flex w-full items-center justify-between px-4 py-3 web:hidden ${isContactPage ? "bg-[#FBFBFC]" : "bg-transparent"} ${isHome ? " h-[67px]" : "h-12"}`}
+      <header
+        className={`pointer-events-auto relative z-[999] flex w-full items-center justify-between px-4 py-3 web:hidden ${
+          isContactPage ? "bg-[#FBFBFC]" : "bg-transparent"
+        } ${isHome ? "h-[67px]" : "h-12"}`}
       >
         {isHome ? (
-          <Link prefetch={false} href="/">
+          <Link href="/">
             <Image
               src="/svg/logo.svg"
               alt={SERVICE_NAME}
@@ -112,13 +121,21 @@ const GlobalNavBar = () => {
           variant="normal"
           size="sm"
         />
-      </div>
+      </header>
 
-      {/* 모바일 메뉴 Drawer */}
       <Drawer open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
-        <DrawerContent className="flex flex-col items-center justify-center gap-y-[59px] bg-white font-pretendard">
+        <DrawerTitle className="sr-only">메뉴 열기</DrawerTitle>
+        <DrawerContent
+          className={`flex flex-col items-center justify-center gap-y-[59px] bg-white font-pretendard`}
+        >
           {NAV_LINKS.map(({ label, path: href }) => (
-            <Link key={href} prefetch={false} href={href} className={`h1-m`}>
+            <Link
+              key={href}
+              prefetch={false}
+              href={href}
+              className="h1-m"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               {label}
             </Link>
           ))}
