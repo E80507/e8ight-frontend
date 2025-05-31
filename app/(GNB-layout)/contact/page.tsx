ec72af8a09b9978fba8b6bfe77d66468ecbfed62
daggle-hyeran <hr.kim@daggle.io>
2025-05-31 18:39:14 +0900

    feat: 문의하기 페이지 반응형 작업

--- app/(GNB-layout)/contact/page.tsx
+++ app/(GNB-layout)/contact/page.tsx
@@ -16,33 +16,27 @@ const ContactPage = () => {
   const formRef = useRef<HTMLFormElement>(null);
 
   return (
-    <div className="bg-[#FBFBFC] tablet:px-[30px] tablet:py-[40px] web:px-[120px] web:py-[40px]">
-      <div className="mx-auto max-w-[1200px] overflow-hidden rounded-[20px]">
+    <div className="web:px-[120px] web:py-[40px] tablet:px-[30px] tablet:py-[40px] bg-[#FBFBFC]">
+      <div className="max-w-[1200px] mx-auto tablet:rounded-[20px] overflow-hidden">
         {/* 배너 */}
         <div
-          className="relative flex h-[357px] items-center overflow-hidden bg-cover bg-center px-[40px] py-[80px]"
-          style={{ backgroundImage: `url("/images/bg-white.webp")` }}
+          className="flex items-center relative overflow-hidden bg-cover bg-center px-[16px] pt-[60px] pb-[16px] tablet:px-[40px] tablet:py-[80px] web:h-[357px] tablet:h-[328px] h-[197px] bg-cover bg-center"
+          style={{ backgroundImage: `url("/images/bg-contact.webp")` }}
         >
           <div className="absolute inset-0 z-0 bg-white opacity-[0.21]" />
 
-          <div className="relative z-10 flex gap-[8px] ">
-            <div className="size-[16px] rounded-full bg-[#70D5B2]" />
-
+          <div className="flex gap-[8px] relative z-10 ">
+            <div className="tablet:w-[16px] tablet:h-[16px] w-[8px] h-[8px] bg-[#70D5B2] rounded-full" />
+            
             <div className="flex flex-col gap-[16px]">
-              <div className="gibson-h1-l">
-                Global No.1
-                <br />
-                Digital Twin Platform
-              </div>
-              <div className="pretendard-body-1">
-                문의를 주시면 자세한 상담이 가능합니다.
-              </div>
+              <div className="web:gibson-h1-l tablet:gibson-h1-m gibson-h1-s">Global No.1<br/>Digital Twin Platform</div>
+              <div className="web:pretendard-body-1 tablet:pretendard-body-2 pretendard-body-3">문의를 주시면 자세한 상담이 가능합니다.</div>
             </div>
           </div>
         </div>
 
         {/* 폼 영역 */}
-        <div className="bg-white p-[40px]">
+        <div className="pt-[16px] pb-[40px] px-[16px] tablet:p-[40px] bg-white">
           <Form {...form}>
             <form
               onSubmit={onSubmit}
@@ -196,11 +190,7 @@ const ContactPage = () => {
               <CustomCheckboxField
                 form={form}
                 name="agreeToReceiveMarketing"
-                label={
-                  <span className="pretendard-body-3">
-                    [선택] 이에이트 뉴스레터 수신에 동의합니다.
-                  </span>
-                }
+                label={<span className="pretendard-body-3">[선택] 이에이트 뉴스레터 수신에 동의합니다.</span>}
               />
 
               {/* 문의하기 버튼 */}
