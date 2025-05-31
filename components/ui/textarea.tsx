39d0e2bbd6ad744d8158210157959e657cd328d5
daggle-hyeran <hr.kim@daggle.io>
2025-05-31 18:39:14 +0900

    feat: 문의하기 페이지 반응형 작업

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        "flex w-full placeholder:text-[#8B909C] rounded-md border border-[#EAECEE] bg-transparent resize-none py-2 px-4 focus-visible:border-none focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 pretendar-body-2",
+        "flex w-full placeholder:text-[#C8C9D0] rounded-md border border-[#EAECEE] bg-transparent resize-none py-2 px-4 focus-visible:border-none focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 pretendar-body-2",
         className,
       )}
       ref={ref}
