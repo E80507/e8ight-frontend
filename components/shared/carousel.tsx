2dbe9b361eda8be0b1d3f263cbd8b2958db2db54
daggle-minseo <ms.choi@daggle.io>
2025-06-01 20:18:01 +0900

    feat: 캐러셀 prev, next 버튼 클릭 시 타이머 초기화

--- components/shared/carousel.tsx
+++ components/shared/carousel.tsx
@@ -8,6 +8,7 @@ import {
   CarouselPrevious,
   type CarouselApi,
 } from "@/components/ui/carousel";
+import { useRef } from "react";
 
 interface CarouselBoxProps {
   setApi: (api: CarouselApi) => void;
@@ -18,51 +19,55 @@ interface CarouselBoxProps {
   }[];
 }
 
-const CarouselBox = ({ setApi, items }: CarouselBoxProps) => (
-  <Carousel
-    opts={{ loop: true }}
-    setApi={setApi}
-    className="flex aspect-[7/8] w-full flex-col  gap-y-[26px] web:w-[calc(100%-384px)] web:min-w-[379px] web:max-w-[482px] web:gap-y-6"
-    plugins={[
-      Autoplay({
-        delay: 3000,
-        stopOnInteraction: false,
-        stopOnMouseEnter: false,
-      }),
-    ]}
-  >
-    <div className="flex items-start justify-between gap-x-2 web:flex-col">
-      <div className="flex items-start gap-x-2 web:hidden">
-        <div className="size-2 rounded-full bg-primary web:size-4" />
-        <h3 className="display-3 tablet:display-2 web:display-1">
-          이에이트의 <br />
-          기술 이야기를
-          <br />
-          전합니다.
-        </h3>
-      </div>
-      <div className="flex gap-x-4 self-end">
-        <CarouselPrevious className="border-none" />
-        <CarouselNext className="border-none" />
-      </div>
-    </div>
-    <CarouselContent>
-      {items.map((_, i) => (
-        <CarouselItem key={i} className="w-full flex-none">
-          <div className="relative aspect-[7/8] w-full overflow-hidden rounded-[20px]">
-            <Image
-              src="/images/history.webp"
-              alt="이미지"
-              fill
-              className="object-cover"
-              sizes="(max-width:600px) 100vw, (max-width: 1025px) 50vw, 33vw"
-              priority
-            />
-          </div>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-  </Carousel>
-);
+const CarouselBox = ({ setApi, items }: CarouselBoxProps) => {
+  const autoplayRef = useRef(
+    Autoplay({
+      delay: 3000,
+      stopOnInteraction: false,
+      stopOnMouseEnter: false,
+    }),
+  );
 
+  return (
+    <Carousel
+      opts={{ loop: true }}
+      setApi={setApi}
+      className="flex aspect-[7/8] w-full flex-col  gap-y-[26px] web:w-[calc(100%-384px)] web:min-w-[379px] web:max-w-[482px] web:gap-y-6"
+      plugins={[autoplayRef.current]}
+      autoplayRef={autoplayRef}
+    >
+      <div className="flex items-start justify-between gap-x-2 web:flex-col">
+        <div className="flex items-start gap-x-2 web:hidden">
+          <div className="size-2 rounded-full bg-primary web:size-4" />
+          <h3 className="display-3 tablet:display-2 web:display-1">
+            이에이트의 <br />
+            기술 이야기를
+            <br />
+            전합니다.
+          </h3>
+        </div>
+        <div className="flex gap-x-4 self-end">
+          <CarouselPrevious className="border-none" />
+          <CarouselNext className="border-none" />
+        </div>
+      </div>
+      <CarouselContent>
+        {items.map((_, i) => (
+          <CarouselItem key={i} className="w-full flex-none">
+            <div className="relative aspect-[7/8] w-full overflow-hidden rounded-[20px]">
+              <Image
+                src="/images/history.webp"
+                alt="이미지"
+                fill
+                className="object-cover"
+                sizes="(max-width:600px) 100vw, (max-width: 1025px) 50vw, 33vw"
+                priority
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+};
 export default CarouselBox;
