cd483f0cb59b324461829814113483b3d407a42c
daggle-minseo <ms.choi@daggle.io>
2025-06-01 17:29:41 +0900

    fix: 페이지 이동시 버튼 태그에 링크 컴포넌트 감싸기

--- components/shared/layout/action-buttons.tsx
+++ components/shared/layout/action-buttons.tsx
@@ -3,27 +3,23 @@
 import SubscriptionModal from "@/app/_components/modal/subscription-modal";
 import { Button } from "@/components/ui/button";
 import { CONTACT_PAGE } from "@/constants/path";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Link from "next/link";
 
 interface ActionButtonsProps {
   className?: string;
 }
 
 const ActionButtons = ({ className }: ActionButtonsProps) => {
-  const router = useRouter();
   const [isSubscriptionModalOpen, setIsSubscriptionModalOpen] = useState(false);
 
   return (
     <div className={className || "mb-[26px] flex gap-x-3"}>
-      <Button
-        size="lg"
-        shape="round"
-        className="bg-black web:w-[160px]"
-        onClick={() => router.push(CONTACT_PAGE)}
-      >
-        상담문의
-      </Button>
+      <Link href={CONTACT_PAGE} prefetch>
+        <Button size="lg" shape="round" className="bg-black web:w-[160px]">
+          상담문의
+        </Button>
+      </Link>
 
       <Button
         size="lg"
