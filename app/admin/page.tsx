b0074ec33756a3a1241b9dd3971772340262ead4
daggle-hyeran <hr.kim@daggle.io>
2025-06-02 18:20:23 +0900

    feat: 모바일 반응형 중간 커밋

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { PostsContainer } from "./table/_components/posts-container";
+import MobilePostsContainer from "./table/_components/mobile/mobile-posts-container"
 
 export default function AdminPage() {
   return (
-    <div className="flex flex-col gap-10 p-10 max-w-[1200px] mx-auto">
-      <PostsContainer />
+    <div className="flex flex-col gap-10 tablet:p-10 max-w-[1200px] mx-auto">
+      {/* pc 화면 */}
+      <div className="hidden md:block">
+        <PostsContainer />
+      </div>
+
+      {/* 모바일 화면  */}
+      <div className="block md:hidden">
+        <MobilePostsContainer />
+      </div>
     </div>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,35 +6,33 @@ import { LoadIcon } from "../shared/loading/loading";
 import Image from "next/image";
 
 const buttonVariants = cva(
-  "flex w-full items-center justify-center whitespace-nowrap rounded-[100px] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:text-black/40",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-white disabled:bg-primary/10",
-        pink: "bg-[#FF6668] text-white",
-        destructive: "bg-destructive text-destructive-foreground",
-        outline: "border bg-white text-black disabled:bg-gray-100",
-        "outline-black": "border border-black disabled:border-gray-300",
+        default:
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-12 px-[15px] heading-4",
-        sm: "h-[30px] max-w-max px-3 py-1 subtitle-3",
-        md: "h-[37px] w-[80px] px-4 py-2 title-s",
-        lg: "h-[48px] w-[120px] px-6 py-3 title-s",
-        cta: "h-[59px] w-[240px] title-s",
-        icon: "size-8",
-      },
-      shape: {
-        round: "rounded-[100px]",
-        square: "rounded-[8px]",
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
       },
     },
     defaultVariants: {
-      shape: "square",
       variant: "default",
       size: "default",
     },
-  },
+  }
 );
 
 export interface ButtonProps
@@ -47,46 +45,16 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      className,
-      variant,
-      size,
-      shape,
-      loading,
-      loadColor,
-      asChild = false,
-      ...props
-    },
-    ref,
-  ) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-
     return (
-      <>
-        {loading ? (
-          <Comp
-            disabled={loading}
-            className={cn(
-              buttonVariants({ variant, size, shape, className }),
-              "relative",
-              loadColor === "white" ? "disabled:border-primary/400" : "",
-            )}
-            ref={ref}
-            {...props}
-          >
-            <LoadIcon type="button" color={loadColor ?? "red"} />
-          </Comp>
-        ) : (
-          <Comp
-            className={cn(buttonVariants({ variant, size, shape, className }))}
-            ref={ref}
-            {...props}
-          />
-        )}
-      </>
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
     );
-  },
+  }
 );
 Button.displayName = "Button";
 
