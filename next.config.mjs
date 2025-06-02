3ba148dd7335b8c53c5f9f46ae3c4e49b4f36ac4
daggle-minseo <ms.choi@daggle.io>
2025-06-02 10:57:47 +0900

    fix: 프록시 제거

--- next.config.mjs
+++ next.config.mjs
@@ -10,14 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
