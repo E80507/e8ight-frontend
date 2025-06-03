436d7c122b5130f8f4341a4d0f1d0b8bc6e27e05
daggle-minseo <ms.choi@daggle.io>
2025-06-03 15:16:52 +0900

    feat: api키 저장하기 위해 admin-auth-gate 생성
    - localStorage에서 adminAuth 키를 사용해 인증 여부를 boolean 값으로 저장하고 관리
    
    - 저장 시 true 또는 false 문자열로 변환하여 저장
    - NEXT_PUBLIC_ADMIN_KEY에 전달받은 api키 값 환경변수로 설정

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import Loading from "@/components/shared/loading/loading";
 import AdminNavBar from "@/components/shared/layout/admin-nav-bar";
 // import { Toaster } from "@/components/ui/toaster";
+import AdminAuthGate from "./_components/admin-auth-gate";
 
 interface GlobalNavBarLayoutProps {
   children: React.ReactNode;
@@ -11,10 +12,12 @@ const AdminLayout = ({ children }: GlobalNavBarLayoutProps) => {
     <>
       <Loading>
         <div className="w-screen overflow-x-hidden">
-          <AdminNavBar />
-          <div className="relative min-h-screen bg-background-alternative">
-            {children}
-          </div>
+          <AdminAuthGate>
+            <AdminNavBar />
+            <div className="relative min-h-screen bg-background-alternative">
+              {children}
+            </div>
+          </AdminAuthGate>
         </div>
       </Loading>
       {/* <Toaster /> */}
