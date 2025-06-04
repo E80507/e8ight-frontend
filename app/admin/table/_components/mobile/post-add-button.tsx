403b741269b4896f867121c5d2e323d5001d6967
daggle-hyeran <hr.kim@daggle.io>
2025-06-04 10:22:26 +0900

    feat: 관리자 pc, mobile 노데이터 추가

--- app/admin/table/_components/mobile/post-add-button.tsx
+++ app/admin/table/_components/mobile/post-add-button.tsx
@@ -8,7 +8,7 @@ const PostAddButton = () => {
   return (
     <button
       type="button"
-      className="fixed bottom-[27px] right-[27px] bg-primary text-white rounded-full size-[56px] flex items-center justify-center cursor-pointer shadow-[0px_4px_5px_0px_#9F9F9F40]"
+      className="fixed bottom-[27px] right-[27px] bg-primary text-white rounded-full size-[56px] flex tablet:hidden items-center justify-center cursor-pointer shadow-[0px_4px_5px_0px_#9F9F9F40]"
       onClick={() => router.push(CREATE_POST_PAGE)}
     >
       <Plus />
