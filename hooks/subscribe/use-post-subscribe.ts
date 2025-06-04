30f77eb911ae9ed892c06f810deb2fbd5171c6e2
daggle-hyeran <hr.kim@daggle.io>
2025-06-04 18:55:58 +0900

    feat: 구독 모달, 구독 섹션 API 연동

--- hooks/subscribe/use-post-subscribe.ts
+++ hooks/subscribe/use-post-subscribe.ts
@@ -3,11 +3,12 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "../use-toast";
 import { z } from "zod";
-import { subscriptionSchema } from "@/schema/subscription";
+import { subsscribeSchema } from "@/schema/subscribe";
+import { postSubscribe } from "@/api/subscribe";
 
-export const usePostSubscription = () => {
-  const form = useForm<z.infer<typeof subscriptionSchema>>({
-    resolver: zodResolver(subscriptionSchema),
+export const usePostSubscribe = (onSuccess?: () => void) => {
+  const form = useForm<z.infer<typeof subsscribeSchema>>({
+    resolver: zodResolver(subsscribeSchema),
   });
 
   const [loading, setLoading] = useState(false); // 로딩 상태
@@ -16,10 +17,15 @@ export const usePostSubscription = () => {
     setLoading(true); // 로딩 시작
 
     try {
+      const email = form.getValues("email");
+      
       // 0.4초 대기
       await new Promise((resolve) => setTimeout(resolve, 400));
+      const response = await postSubscribe(email);
+      onSuccess?.();
     } catch (err: unknown) {
       if (!err) return;
+      console.error("Subscribe error:", err);
       toast({
         title: "잠시 후 다시 시도해주세요",
         // icon: "light",
