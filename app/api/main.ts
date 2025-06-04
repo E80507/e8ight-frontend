e4a570907937dbe268294b8ecad0b6da133d3983
daggle-minseo <ms.choi@daggle.io>
2025-06-04 19:16:52 +0900

    feat: 기술 이야기 캐러셀 데이터 api 바인딩

--- app/api/main.ts
+++ app/api/main.ts
@@ -2,10 +2,10 @@ import { CarouselRes } from "./dto/main";
 import { apiFetch } from "@/util/fetch";
 
 // 캐러셀 정보 조회
-export const getCarouselData = async (): Promise<() => CarouselRes[]> => {
+export const getHistoryData = async (): Promise<CarouselRes[]> => {
   try {
-    const res = await apiFetch(""); // 수정 필요
-    return res;
+    const res = await apiFetch("/posts?page=1&limit=3&sortOrder=RANDOM");
+    return res.posts;
   } catch (err) {
     console.error(err);
     throw new Error("캐러셀 조회에 실패했습니다.");
