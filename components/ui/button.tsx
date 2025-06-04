44eee7c0b4c6fdf1a4872b435b527c43ca893e94
daggle-hyeran <hr.kim@daggle.io>
2025-06-04 21:14:33 +0900

    feat: 어드민 상세 페이지 반응형 작업 및 데이터 바인딩

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         "outline-black": "border border-black disabled:border-gray-300",
       },
       size: {
-        default: "h-12 px-[15px] heading-4",
+        default: "h-12 px-[15px] pretendard-title-s",
         sm: "h-[30px] max-w-max px-3 py-1 subtitle-3",
         md: "h-[37px] w-[80px] px-4 py-2 title-s",
         lg: "h-[48px] w-[120px] px-6 py-3 title-s",
