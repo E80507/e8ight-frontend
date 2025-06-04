a6cf5f361285f801e3c4ac7ba1d2f6426868a733
daggle-minseo <ms.choi@daggle.io>
2025-06-04 11:06:23 +0900

    feat: 어드민 모바일 gnb 추가

--- components/shared/layout/admin-nav-bar.tsx
+++ components/shared/layout/admin-nav-bar.tsx
@@ -1,30 +1,64 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { SERVICE_NAME } from "@/constants/service";
 import { Button, IconButton } from "@/components/ui/button";
 import { ADMIN_PAGE } from "@/constants/path";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const AdminNavBar = () => {
-  return (
-    <header className="relative mx-auto flex w-full max-w-[1440px] items-center justify-between p-4 font-pretendard web:px-[120px] web:py-6">
-      {/* 왼쪽: 로고 */}
-      <Link
-        type="button"
-        href={ADMIN_PAGE}
-        className="flex items-center"
-        prefetch
-      >
-        <Image
-          src="/svg/logo.svg"
-          alt={SERVICE_NAME}
-          width={64}
-          height={43}
-          priority
-        />
-        <span className="font-gibson h2-r">E8 Admin</span>
-      </Link>
+  const router = useRouter();
+  const path = usePathname();
+  const isCreatePage = path === "/admin/create-post";
 
-      {/* 오른쪽: 버튼 + 햄버거 아이콘 */}
+  return (
+    <header
+      className={`relative mx-auto flex w-full max-w-[1440px] items-center justify-between p-4 font-pretendard web:px-[120px] web:py-6`}
+    >
+      {/* 왼쪽 영역 */}
+      <div className="flex items-center web:gap-4">
+        {/* 모바일 뒤로가기 */}
+        <IconButton
+          src="/svg/icon/chevron-left.svg"
+          width={24}
+          height={24}
+          onClick={() => {
+            if (window.history.length > 1) {
+              router.back();
+            } else {
+              router.push(ADMIN_PAGE);
+            }
+          }}
+          variant="normal"
+          size="sm"
+          className="flex web:hidden"
+        />
+
+        {/* 데스크톱 로고 */}
+        <Link
+          href={ADMIN_PAGE}
+          className="hidden items-center gap-2 web:flex"
+          prefetch
+        >
+          <Image
+            src="/svg/logo.svg"
+            alt={SERVICE_NAME}
+            width={64}
+            height={43}
+            priority
+          />
+          <span className="font-gibson h2-r">E8 Admin</span>
+        </Link>
+      </div>
+
+      {/* 가운데 타이틀 (모바일 전용) */}
+      <p className="flex-1 text-center pretendard-title-m web:hidden">
+        {isCreatePage ? "컨텐츠 추가" : ""}
+      </p>
+
+      {/* 오른쪽 영역 */}
       <div className="flex items-center gap-4">
         <Link href={"/"} prefetch>
           <Button
@@ -42,7 +76,7 @@ const AdminNavBar = () => {
           height={24}
           variant="normal"
           size="sm"
-          className="flex web:hidden"
+          className={`flex web:hidden ${isCreatePage ? "hidden" : ""}`}
         />
       </div>
     </header>
