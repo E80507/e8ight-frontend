9370ab0a64da347f9b7f1a967504ea012e49805b
daggle-minseo <ms.choi@daggle.io>
2025-06-04 19:56:08 +0900

    feat: Dx 시뮬레이션 api 바인딩

--- app/api/dto/main.ts
+++ app/api/dto/main.ts
@@ -1,8 +1,14 @@
-export interface CarouselRes {
+export interface HistoryRes {
   id: string;
   title: string;
   description: string;
   thumbnail: string;
   link: string;
   category: string;
+  createdAt: string;
+}
+export interface SiumlationRes {
+  description: string;
+  thumbnail: string;
+  createdAt: string;
 }
--- app/api/main.ts
+++ app/api/main.ts
@@ -1,10 +1,23 @@
-import { CarouselRes } from "./dto/main";
+import { HistoryRes, SiumlationRes } from "./dto/main";
 import { apiFetch } from "@/util/fetch";
 
-// 캐러셀 정보 조회
-export const getHistoryData = async (): Promise<CarouselRes[]> => {
+// 기술 이야기 정보 조회
+export const getHistoryData = async (): Promise<HistoryRes[]> => {
   try {
     const res = await apiFetch("/posts?page=1&limit=3&sortOrder=RANDOM");
+    return res.posts;
+  } catch (err) {
+    console.error(err);
+    throw new Error("캐러셀 조회에 실패했습니다.");
+  }
+};
+
+// 캐러셀 정보 조회
+export const getSimulationData = async (): Promise<SiumlationRes[]> => {
+  try {
+    const res = await apiFetch(
+      "/posts?page=1&limit=9&category=DX&sortOrder=RANDOM",
+    );
     return res.posts;
   } catch (err) {
     console.error(err);
