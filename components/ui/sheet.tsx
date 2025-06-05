3d759d01f7517bc8b20f7c93d51188712f560f52
daggle-jisu <js.park@daggle.io>
2025-06-05 17:28:07 +0900

    chore: prettier formatting

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -31,7 +31,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
   {
     variants: {
       side: {
@@ -80,7 +80,7 @@ const SheetHeader = ({
   >
     <div className="flex items-center">{props.children}</div>
     <SheetPrimitive.Close className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-      <X className="h-4 w-4" />
+      <X className="size-4" />
       <span className="sr-only">Close</span>
     </SheetPrimitive.Close>
   </div>
