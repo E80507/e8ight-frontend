66e13761f0192debfc9e3802b6cf621848a70e35
daggle-hyeran <hr.kim@daggle.io>
2025-06-05 18:02:31 +0900

    feat: 다운로드 파일명 데이터 바인딩

--- components/shared/form/custom-file-upload-field.tsx
+++ components/shared/form/custom-file-upload-field.tsx
@@ -1,11 +1,23 @@
+import { formatBytes } from "@/util/file";
+import Image from "next/image";
+
+interface FileInfo {
+  id: string;
+  fileName: string;
+  fileSize: number;
+  fileUrl: string;
+}
+
 interface CustomFileUploadFieldProps {
   label?: string;
   isEssential?: boolean;
+  files?: FileInfo[];
 }
 
 const CustomFileUploadField = ({
   label,
   isEssential,
+  files,
 }: CustomFileUploadFieldProps) => {
   return (
     <div>
@@ -22,14 +34,29 @@ const CustomFileUploadField = ({
           <div>용량</div>
         </div>
 
-        <div className="flex h-[45px] items-center justify-between px-[16px] py-[12px]">
-          <div className="flex items-center gap-[10px]">
-            <div className="size-[24px] rounded-[2px] bg-background-natural"></div>
-            <div className="body-3">파일 제목이 들어갑니다.</div>
-          </div>
+        {files && files.length > 0 ? (
+          files.map((file) => (
+            <div key={file.id} className="flex h-[45px] items-center justify-between px-[16px] py-[12px]">
+              <div className="flex items-center gap-[10px]">
+                <div className="relative size-[24px] overflow-hidden rounded-[2px]">
+                  <Image
+                    src={file.fileUrl}
+                    alt={file.fileName}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="pretendard-body-3">{file.fileName}</div>
+              </div>
 
-          <div className="text-label-normal body-3">1.93MB</div>
-        </div>
+              <div className="text-label-normal body-3">{formatBytes(file.fileSize)}</div>
+            </div>
+          ))
+        ) : (
+          <div className="flex h-[45px] items-center justify-between px-[16px] py-[12px]">
+            <div className="text-label-assistive body-2">등록된 파일이 없습니다.</div>
+          </div>
+        )}
       </div>
     </div>
   );
