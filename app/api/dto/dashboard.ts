c8f881b899168fb237ae8d19332919ccd1eaf2cc
daggle-minseo <ms.choi@daggle.io>
2025-06-05 10:44:51 +0900

    fix: 카테고리별로 게시글 검색, 조회되도록 api 바인딩

--- app/api/dto/dashboard.ts
+++ app/api/dto/dashboard.ts
@@ -1,13 +1,14 @@
 // 포스트 검색
 export interface SearchReq {
-  query: string;
-  searchFields: string[];
   page: number;
   limit: number;
+  category: string;
+  keyword: string;
 }
 
 // 포스트 전체 조회
 export interface GetPostsReq {
   page: number;
   limit: number;
+  category: string;
 }
