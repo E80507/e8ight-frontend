d7334fadb04f975a0f8db1a043743165b58ecc38
daggle-minseo <ms.choi@daggle.io>
2025-06-07 11:38:59 +0900

    Reapply "Merge branch 'main' into fix/hero-section"
    This reverts commit c4553bf62d60abec0fcc640a9c07db7f83d31eb9.

--- app/(GNB-layout)/detail/[id]/page.tsx
+++ app/(GNB-layout)/detail/[id]/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-
 import DetailContent from "./_components/detail-content";
 
-interface DetailPageProps {
-  params: { id: string };
-}
+const PostDetailPage = () => {
+  return <DetailContent />;
+};
 
-export default function Page({ params }: DetailPageProps) {
-  return <DetailContent params={params} />;
-}
+export default PostDetailPage;
--- components/shared/form/custom-file-upload-field.tsx
+++ components/shared/form/custom-file-upload-field.tsx
@@ -6,11 +6,12 @@ interface CustomFileUploadFieldProps {
 const CustomFileUploadField = ({
   label,
   isEssential,
+  files,
 }: CustomFileUploadFieldProps) => {
   return (
     <div>
       {label && (
-        <label className="pretendard-subtitle-m mb-[12px] block">
+        <label className="mb-[12px] block pretendard-subtitle-m">
           {label}
           {isEssential && <span className="ml-[2px] text-error">{`*`}</span>}
         </label>
@@ -27,9 +28,7 @@ const CustomFileUploadField = ({
             <div className="w-[24px] h-[24px] rounded-[2px] bg-background-natural"></div>
             <div className="body-3">파일 제목이 들어갑니다.</div>
           </div>
-
-          <div className="body-3 text-label-normal">1.93MB</div>
-        </div>
+        )}
       </div>
     </div>
   );
