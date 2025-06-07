06db32299a6918439fe24a1fe08cdc0e20821a46
daggle-minseo <ms.choi@daggle.io>
2025-06-07 12:06:43 +0900

    fix: 메인 텍스트 line height 140%로 변경

--- app/main/hero-section.tsx
+++ app/main/hero-section.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <section className="relative mx-auto h-[calc(100vh-67px)] w-full max-w-[1440px] px-4 tablet:px-[30px] web:h-[calc(100vh-139px)] web:px-[120px] web:pt-[100px]">
       <div className="absolute bottom-0 mb-[100px] flex flex-col gap-x-2 text-center text-white ">
         <div className="mb-8 flex flex-col gap-y-3">
-          <h1 className="text-left text-white gibson-h1-s tablet:gibson-h1-m web:gibson-h1-l">
+          <h1 className="text-left !leading-[140%] text-white gibson-h1-s tablet:gibson-h1-m web:gibson-h1-l">
             Global No.1
             <br />
             Digital Twin Platform
