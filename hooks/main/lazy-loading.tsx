80af526fb21c6a3ca72bf29237dd994068f177df
daggle-minseo <ms.choi@daggle.io>
2025-06-09 16:05:49 +0900

    feat: 홈페이지 히어로 섹션 제외하고 레이지 로딩 적용

---
+++ hooks/main/lazy-loading.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode, useEffect, useRef, useState } from "react";
+
+const LazyLoad = ({ children }: { children: ReactNode }) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const isMobile = window.innerWidth < 768;
+    const rootMargin = isMobile ? "0px 0px -50px 0px" : "0px 0px -100px 0px";
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsIntersecting(true);
+          observer.disconnect();
+        }
+      },
+      {
+        root: null,
+        rootMargin: rootMargin,
+        threshold: 0,
+      },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return <div ref={ref}>{isIntersecting ? children : null}</div>;
+};
+
+export default LazyLoad;
