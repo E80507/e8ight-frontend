ef002ee83fe39ffac47cb188d63d91c07ad174b4
daggle-minseo <ms.choi@daggle.io>
2025-06-09 20:01:21 +0900

    feat: amplitude 연결
    - 문의하기 버튼, 다운로드 버튼, 1:1 상담, 뉴스 레터 구독 신청 버튼

--- app/(GNB-layout)/page.tsx
+++ app/(GNB-layout)/page.tsx
@@ -6,8 +6,23 @@ import SubscriptionSection from "../main/subscription-section";
 import HistorySection from "../main/history-section";
 import SimulationSection from "../main/simulation.section";
 import LazyLoad from "@/hooks/main/lazy-loading";
+// import {
+//       AMPLITUDE_EVENT_DISPLAY_NAME,
+//       AMPLITUDE_EVENT_LOG_NAME,
+// } from "@/constant/amplitude";
 
 const Home = () => {
+  // todo: amplitude 추가 후 사용
+  // const { trackPageView } = useTrackPageViewWithUTM();
+
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return;
+  //   trackPageView(
+  //     AMPLITUDE_EVENT_LOG_NAME.MAIN_PAGE_VIEW,
+  //     AMPLITUDE_EVENT_DISPLAY_NAME.MAIN_PAGE_VIEW,
+  //   );
+  // }, [trackPageView]);
+
   return (
     <>
       {/* 히어로 섹션 */}
---
+++ constants/amplitude.ts
@@ -0,0 +1,11 @@
+export const AMPLITUDE_EVENT_DISPLAY_NAME = {
+  // mainpage
+  LANDING_PAGE_VIEW: "랜딩페이지 조회",
+  CTA_BUTTON_CLICK: "CTA 버튼 클릭",
+};
+
+export const AMPLITUDE_EVENT_LOG_NAME = {
+  // mainpage
+  LANDING_PAGE_VIEW: "landing_page_view",
+  CTA_BUTTON_CLICK: "landing_page_cta_button_click",
+};
