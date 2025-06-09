183e22ca5183c2240764936068547bf7f161f22d
daggle-hyeran <hr.kim@daggle.io>
2025-06-09 11:48:50 +0900

    fix: pdf download 모달에서 연락처 추가

--- app/(GNB-layout)/detail/[id]/_components/post-content.tsx
+++ app/(GNB-layout)/detail/[id]/_components/post-content.tsx
@@ -9,7 +9,7 @@ interface PostContentProps {
 
 export default function PostContent({ post }: PostContentProps) {
   return (
-    <div className="tablet:mt-[40px] mt-[24px]">
+    <div className="tablet:mt-[40px] mt-[24px] admin-detail-content">
       <QuillViewer content={post?.content} />
     </div>
   );
