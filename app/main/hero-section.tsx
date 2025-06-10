7740c2a78c7e5824ad324333b5f90c554c6a8171
daggle-hyeran <hr.kim@daggle.io>
2025-06-10 10:24:45 +0900

    feat: 히어로 섹션에서도 구독 모달 추가

--- app/main/hero-section.tsx
+++ app/main/hero-section.tsx
@@ -1,6 +1,10 @@
 import ActionButtons from "@/components/shared/layout/action-buttons";
+import { useState } from "react";
+import SubscriptionModal from "../_components/modal/subscription-modal";
 
 const HeroSection = () => {
+  const [isSubscriptionModalOpen, setIsSubscriptionModalOpen] = useState(false);
+  
   return (
     <section className="relative mx-auto min-h-screen w-full max-w-[1440px] px-4 tablet:px-[30px] web:h-[calc(100vh-139px)] web:px-[120px] web:pt-[100px]">
       <div className="absolute bottom-0 mb-[100px] flex flex-col gap-x-2 text-center text-white ">
@@ -16,7 +20,18 @@ const HeroSection = () => {
             Transformation
           </h2>
         </div>
-        <ActionButtons className="border-black-1 bg-white text-black hover:border-primary hover:bg-primary" />
+        
+        <ActionButtons 
+          className="border-black-1 bg-white text-black hover:border-primary hover:bg-primary"
+          isSubscriptionModalOpen={isSubscriptionModalOpen}
+          setIsSubscriptionModalOpen={setIsSubscriptionModalOpen}
+        />
+      
+        {isSubscriptionModalOpen && (
+          <SubscriptionModal
+            onClickClose={() => setIsSubscriptionModalOpen(false)}
+          />
+        )}
       </div>
     </section>
   );
