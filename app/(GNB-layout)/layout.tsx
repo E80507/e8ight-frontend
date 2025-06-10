2268d522b6f76975a8955be49a93061f16a82c8d
daggle-minseo <ms.choi@daggle.io>
2025-06-10 12:59:17 +0900

    fix: 랜딩 페이지 로딩 스피너 적용

--- app/(GNB-layout)/layout.tsx
+++ app/(GNB-layout)/layout.tsx
@@ -1,24 +1,15 @@
-"use client";
-
 import GlobalNavBar from "@/components/shared/layout/global-nav-bar";
 import GlobalFooter from "@/components/shared/layout/global-footer";
-import BackgroundSection from "../main/background-section";
-import { usePathname } from "next/navigation";
 
 interface GlobalNavBarLayoutProps {
   children: React.ReactNode;
 }
 
 const GlobalNavBarLayout = ({ children }: GlobalNavBarLayoutProps) => {
-  const pathname = usePathname();
-  const isMain = pathname === "/";
   return (
     <>
       <div className="w-screen overflow-x-hidden">
         <GlobalNavBar />
-        {/* 백그라운드 섹션 */}
-        {isMain && <BackgroundSection />}
-
         <div className="relative min-h-screen">{children}</div>
         <GlobalFooter />
       </div>
